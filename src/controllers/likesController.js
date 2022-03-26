4abe792d7992c5adef5a7c6efb8c4ac53656415c
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-03-25 22:42:33 -0300

    feat: toggle like

--- src/controllers/likesController.js
+++ src/controllers/likesController.js
@@ -1,41 +1,66 @@
-import connection from "../database.js";
+import connection from '../database.js';
 
 export async function toggleLike(req, res) {
-    const {postId, userId} = req.body;
-    console.log(postId)
-    console.log(userId)
+  const { postId } = req.body;
+  const userId = res.locals.userId
+  console.log(userId);
 
-    try {
-        await connection.query(`
-        INSERT INTO likes
-            ("postId", "userId")
-        VALUES ($1, $2)
-        `, [postId, userId])
-    res.sendStatus(200)
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
+  try {
+    const result = await connection.query(
+      `
+        SELECT * FROM likes
+            WHERE "postId" = $1 AND "userId" = $2
+        `,
+      [postId, userId]
+    );
+
+    console.log(result.rows);
+    if (result.rowCount !== 0) {
+      await connection.query(
+        `
+                DELETE FROM likes
+                WHERE "postId" = $1 AND "userId" = $2
+        `,
+        [postId, userId]
+      );
+    } else {
+      await connection.query(
+        `
+            INSERT INTO likes
+                ("postId", "userId")
+            VALUES ($1, $2)
+            `,
+        [postId, userId]
+      );
     }
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 }
 
 export async function getLikes(req, res) {
-    const { postId } = req.params;
-    console.log(postId)
-    //retornar 2 likes mais recentes
-    //retornar count de likes no post
+  const { postId } = req.params;
+  console.log(postId);
+  //retornar 2 likes mais recentes
+  //retornar count de likes no post
 
-    try {
-        await connection.query(`
+  try {
+    await connection.query(
+      `
         SELECT
             users.name AS users
             "postId"
         FROM likes
         JOIN users ON users.id = likes."userId"
         WHERE postId = $1
-        `, [postId]
-        )
-    } catch(error) {
-        console.log(error);
-        res.status(500).send(error);
-    }
+        `,
+      [postId]
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 }
