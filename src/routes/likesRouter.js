61c638e0957c09ccdb998f25e65d9db5549175f4
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-03-27 18:21:26 -0300

    feat: get user likes

--- src/routes/likesRouter.js
+++ src/routes/likesRouter.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 
 import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 
-import { toggleLike, getLikes } from "../controllers/likesController.js";
+import { toggleLike, getLikes, getUserLikes } from "../controllers/likesController.js";
 
 const likesRouter = Router();
 likesRouter.post('/like', validateTokenMiddleware, toggleLike);
 likesRouter.post('/likes', validateTokenMiddleware, getLikes);
+likesRouter.get('/likes', validateTokenMiddleware, getUserLikes)
 
 export default likesRouter;
