58abd261f405e94864fc012a3cfbeadd71765a92
Sophia de Lisieux Zocoler <sophiazocoler@gmail.com>
2022-03-27 23:47:39 -0300

    feat: get posts by hashtag

--- src/controllers/hashtagsController.js
+++ src/controllers/hashtagsController.js
@@ -4,6 +4,14 @@ export function getPostsByHashtag(req, res) {
     const { hashtag } = req.params;
 
     try {
+        const result = connection.query(`
+        SELECT "postsId", hashtags.name AS hashtags
+        FROM hashtagsPosts
+        JOIN hashtags on hashtagsPosts."hashtagId"=hashtags.id
+        WHERE hashtags.name=$1
+        `, [hashtag])
+
+        res.send(result.rows)
 
     } catch (error) {
         console.log(error)
